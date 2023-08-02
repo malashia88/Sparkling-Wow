cad0bb471acdca81eb24b7f802dd11f809955b9d
malashia88 <140565670+malashia88@users.noreply.github.com>
2023-08-02 18:49:21 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome To Calculating The Cost of Installing Fiber Optic Cable")
+company=input("Enter company name:")
+feets=float(input("Enter number of feet of fiber optic to be installed:"))
+cost=feets*0.87
+print("Company Name:"+company)
+print("cost:"+str(cost))
